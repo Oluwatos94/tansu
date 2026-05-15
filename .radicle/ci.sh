5510182b00b5b6854c8fc37945cc6087b6e2ee9f
Tupui <23188539+tupui@users.noreply.github.com>
2026-05-15 17:34:52 +0200

    Cleanup local orphan ci branches

--- .radicle/ci.sh
+++ .radicle/ci.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-repo_root=$(git rev-parse --show-toplevel)
-cd "$repo_root"
+cd "$(git rev-parse --show-toplevel)"
 
 rad_ci_branch=rad/ci
 rid=$(rad inspect --rid)
@@ -12,22 +11,22 @@ keep=90
 log=.radicle/ci/${sha}.log
 
 rad_push_url=$(git config --get remote.rad.pushurl)
-rad_remote_nid=${rad_push_url##*/}
-uri=https://radicle.network/nodes/$node/$rid/remotes/$rad_remote_nid/tree/$rad_ci_branch/$log
+rad_namespace=${rad_push_url##*/}
+uri=https://radicle.network/nodes/$node/$rid/remotes/$rad_namespace/tree/$rad_ci_branch/$log
 
 git fetch rad
 
-disposable_orphan_branch=radicle-ci-$$
+# new worktree with orphan branch to run CI and create log
 ci_worktree_dir=$(mktemp -d)
 git worktree add --detach "$ci_worktree_dir" HEAD
 
 (
   cd "$ci_worktree_dir"
-  git checkout --orphan "$disposable_orphan_branch"
+  git checkout --orphan "radicle-ci-$$"
   git rm -rf . >/dev/null
   git clean -fd
 
-  # first push: remote may not have rad/ci yet
+  # first push: remote may not have rad/ci yet, pull existing logs otherwise
   git checkout "rad/$rad_ci_branch" -- .radicle/ci || true
   mkdir -p .radicle/ci
 
@@ -39,19 +38,20 @@ if ! rad-job show $sha >/dev/null; then rad-job new $sha; fi
 run=$(rad-job run $sha $uri)
 
 # CI job itself
-{ make contract_build && make contract_test; } 2>&1 | tee -a "$ci_worktree_dir/$log"
+{ cargo test; } 2>&1 | tee -a "$ci_worktree_dir/$log"
 
 # outcome
 rc=${PIPESTATUS[0]}
 if [ $rc -eq 0 ]; then status=succeeded; else status=failed; fi
 rad job $status $sha $run
 
-# cleanup branch and worktree
+# save log to branch, then cleanup branch and worktree
 (
   cd "$ci_worktree_dir"
   git add -f .radicle/ci
   git commit --no-verify -m "ci $status ${sha:0:7}"
   git push --force rad "HEAD:refs/heads/$rad_ci_branch"
+  git update-ref -d "refs/heads/radicle-ci-$$" 2>/dev/null || true
 )
 git worktree remove -f "$ci_worktree_dir"
 
